c8cff0cd4519c045b7b07180aab04dc059bb7b24
luizotavioandrade <163910069+luizotavioandrade@users.noreply.github.com>
2024-07-10 16:37:48 -0300

    ss

--- ValorX.cs
+++ ValorX.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Atividade4do7
 {
-    internal class ValorX
+    public class ValorX
     {
+        public int X { get; set; }
+        public void valorX()
+        {
+            Console.WriteLine("valor para X");
+            X = int.Parse(Console.ReadLine());
+
+            if (X < 0)
+            {
+                Console.WriteLine(X);
+            }
+            else if (X > 0)
+            {
+                int b = X * (-1);
+                Console.WriteLine(b);
+            }
+        }
     }
 }
